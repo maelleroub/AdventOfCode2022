697b943cd7dfd4459b722ee2082063532a1ca43e
maelleroub <maelle.roubeau@epita.fr>
2022-12-11 16:22:31 +0100

    swap day1 A and B

--- src/bin/day1_b.rs
+++ src/bin/day1_b.rs
@@ -1,14 +1,13 @@
 fn main() {
     let input = include_str!("day1_input");
     let lines : Vec<&str> = input.lines().collect();
-    let mut max = 0;
+    let mut calories: Vec<i32> = Vec::new();
     let mut cur = 0;
 
     for line in lines {
-        if line.is_empty() {
-            if cur > max {
-                max = cur;
-            }
+        if line.is_empty()
+        {
+            calories.push(cur);
             cur = 0;
         }
         else {
@@ -16,5 +15,6 @@ fn main() {
         }
     }
 
-    println!("{}", max);
+    calories.sort_by(|a, b| b.cmp(a));
+    println!("{}", calories[0] + calories[1] + calories[2]);
 }
